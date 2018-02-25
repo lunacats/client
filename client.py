11227ba28dbe4009a97f7ccb76cd7c1efcf95419
harshrealm <=>
2018-02-25 14:56:06 -0500

    whatever

--- client.py
+++ client.py
@@ -26,15 +26,14 @@ import pygame
 
 PORT = 8000
 IP = "127.0.0.1"
-MESSAGE = "hello"
 
 # test sending a message
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (IP, PORT))
+#sock.sendto(MESSAGE, (IP, PORT))
 
 pygame.init()  # init pygame
 pygame.joystick.init()  # init pygame joysticks
-joysticks = pygame.joystick.Joystick(x)
+joysticks = pygame.joystick.Joystick(0)
 
 joycount = pygame.joystick.get_count()
 print( joycount )
@@ -67,28 +66,40 @@ for x in range(0, joycount):
 def handlebutton(number, value):
     if value == 0:  # button has been pressed. most methods will activate on this condition
         if number == 0:   # on the steelseries gamepad, this in the top button on the right
+            print("1 pressed")
             senddata(b'\xB1')
         elif number == 1: # on the steelseries gamepad, this in the right button on the right
+            print("2 pressed")
             senddata(b'\xB2')
         elif number == 2: # on the steelseries gamepad, this in the bottom button on the right
+            print("3 pressed")
             senddata(b'\xB3')
         elif number == 3: # on the steelseries gamepad, this in the left button on the right
+            print("4 pressed")
             senddata(b'\xB4')
         elif number == 4: # on the steelseries gamepad, this in the trigger button on the left
+            print("5 pressed")
             senddata(b'\xB5')
         elif number == 5: # on the steelseries gamepad, this in the trigger button on the right
+            print("6 pressed")
             senddata(b'\xB6')
         elif number == 6: # on the steelseries gamepad, this in the bumper button on the left
+            print("7 pressed")
             senddata(b'\xB7')
         elif number == 7: # on the steelseries gamepad, this in the bumper button on the right
+            print("8 pressed")
             senddata(b'\xB8')
         elif number == 8: # on the steelseries gamepad, this is the left middle button
+            print("9 pressed")
             senddata(b'\xB9')
         elif number == 9: # on the steelseries gamepad, this is the right middle button
+            print("10 prssed")
             senddata(b'\xB0')
         elif number == 10: # on the steelseries gamepad, this is pressing the left control stick in
+            print("11 pressed")
             senddata(b'\xBA')
         elif number == 11: # on the steelseries gamepad, this is pressing the right control stick in
+            print("12 pressed")
             senddata(b'\xBB')
         else:
             print("This isn't a supported button!")
@@ -98,25 +109,25 @@ def handlebutton(number, value):
 def handleaxis(number, value):
     if number == 0: # left stick's left to right
         if value < -0.5:
-            print("Moving left")
+            print("Left stick to the left")
             senddata( b'\xD9')
         elif value > 0.5:
-            print("Moving right")
+            print("Left stick to the right")
             senddata( b'\xD7')
         #else:
             #   senddata( b'\xD1')
             #   print("Left stick LR neutral")
-    if number == 1: # left stick's up to down
+    elif number == 1: # left stick's up to down
         if value < -0.5:
-            print("Moving forward")
+            print("Left stick forward")
             senddata( b'\xD6')
         elif value > 0.5:
-            print("Moving backward")
+            print("Left stick backward")
             senddata( b'\xD8')
         #else:
             #    senddata( b'\xD5')
             #    print("Left stick UD neutral")
-    if number == 2: # right stick's left to right
+'''     elif number == 2: # right stick's left to right
         if value < -0.5:
             print("Right stick left")
             senddata( b'\xDE')
@@ -126,7 +137,7 @@ def handleaxis(number, value):
         #else:
             #    senddata( b'\xDA')
             #    print("Right stick LR neutral")
-    if number == 3: # right stick's up to down
+    elif number == 3: # right stick's up to down
         if value < -0.5:
             senddata( b'\xDB')
             print("Right stick up")
@@ -135,30 +146,23 @@ def handleaxis(number, value):
             print("Right stick down")
         #else:
             #senddata( b'\xDA')
-            #    print("Right stick UD neutral")
+            #    print("Right stick UD neutral") '''
 
 
 def handlehat(number, value):
     if number == 0: # left stick's left to right
-        if value < -0.5:
-            print("Moving left")
-            senddata( b'\xD4')
-        elif value > 0.5:
-            print("Moving right")
-            senddata( b'\xD2')
-        #else:
-            #    senddata( b'\xD0')
-            #    print("Left hat LR neutral")
-    if number == 1: # left stick's up to down
-        if value < -0.5:
-            print("Moving forward")
-            senddata( b'\xD1')
-        elif value > 0.5:
-            print("Moving backward")
-            senddata( b'\xD3')
-        #else:
-            #   senddata( b'\xD0')
-            #    print("Left hat UD neutral")
+        if value[0] == 0 and value[1] == 1:
+            print("DPad up")
+            senddata(b'\xD1')
+        elif value[0] == 1 and value[1] == 0:
+            print("DPad right")
+            senddata(b'xD2')
+        elif value[0] == 0 and value[1] == -1:
+            print("DPad down")
+            senddata(b'xD3')
+        elif value[0] == -1 and value[1] == 0:
+            print("DPad left")
+            senddata(b'xD4')
 
 
 def handlecontrol( number, value, conttype = "button" ):
