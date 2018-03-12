51c3405b8ccca194109ae92f625ac47e0a1c7b4e
Garrett  Wedge, cs619-s17 <gjd36@wildcats.unh.edu>
2018-03-12 15:18:17 -0400

    cleaned up some small errors and added mode docstrings

--- client.py
+++ client.py
@@ -2,23 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #  client.py
-#  
-#  Copyright 2018 Garrett J Wedge <gjd36@lapaz.cs.unh.edu>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
+#
 #  
 #  
 import socket
@@ -37,7 +21,7 @@ joysticks = pygame.joystick.Joystick(0)
 
 joycount = pygame.joystick.get_count()
 print( joycount )
-for x in range(0, joycount):
+for x in (0, joycount):
     print(joysticks)  # print list of connected controllers
 
 # The steelseries gamepad used to set this up has the following characteristics
@@ -53,9 +37,8 @@ for x in range(0, joycount):
 # and number that represents in in the code, m.
 # m = n - 1
 # If the button says 1 on it, then the code has it as button 0
-# There are 10 buttons in this manner
+# There are 12 buttons in this manner
 #
-# There's also a d-pad, which I think pygame calls a "hat", but it hasn't been tested yet
 #
 # This code will be compatible with other controllers, but the behavior is not guaranteed,
 # don't go to competition without verifying the controller works the way it is expected to
@@ -64,6 +47,11 @@ for x in range(0, joycount):
 
 
 def handlebutton(number, value):
+    """
+    :param number: the number of the button, as represented in the code (m)
+    :param value: pressed or released
+    :return:
+    """
     if value == 0:  # button has been pressed. most methods will activate on this condition
         if number == 0:   # on the steelseries gamepad, this in the top button on the right
             print("1 pressed")
@@ -125,18 +113,18 @@ def handleaxis(number, value):
             print("Left stick backward")
             senddata( b'\xD8')
         #else:
-            #    senddata( b'\xD5')
-            #    print("Left stick UD neutral")
-'''     elif number == 2: # right stick's left to right
-        if value < -0.5:
-            print("Right stick left")
-            senddata( b'\xDE')
-        elif value > 0.5:
-            print("Right stick right")
-            senddata( b'\xDC')
+        #    senddata( b'\xD5')
+        #    print("Left stick UD neutral")
+        elif number == 2: # right stick's left to right
+            if value < -0.5:
+                print("Right stick left")
+                senddata( b'\xDE')
+            elif value > 0.5:
+                print("Right stick right")
+                senddata( b'\xDC')
         #else:
-            #    senddata( b'\xDA')
-            #    print("Right stick LR neutral")
+        #    senddata( b'\xDA')
+        #    print("Right stick LR neutral")
     elif number == 3: # right stick's up to down
         if value < -0.5:
             senddata( b'\xDB')
@@ -145,24 +133,25 @@ def handleaxis(number, value):
             senddata( b'\xDD')
             print("Right stick down")
         #else:
-            #senddata( b'\xDA')
-            #    print("Right stick UD neutral") '''
+        #senddata( b'\xDA')
+        #    print("Right stick UD neutral") '''
 
 
 def handlehat(number, value):
+
     if number == 0: # left stick's left to right
         if value[0] == 0 and value[1] == 1:
             print("DPad up")
             senddata(b'\xD1')
         elif value[0] == 1 and value[1] == 0:
             print("DPad right")
-            senddata(b'xD2')
+            senddata(b'\xD2')
         elif value[0] == 0 and value[1] == -1:
             print("DPad down")
-            senddata(b'xD3')
+            senddata(b'\xD3')
         elif value[0] == -1 and value[1] == 0:
             print("DPad left")
-            senddata(b'xD4')
+            senddata(b'\xD4')
 
 
 def handlecontrol( number, value, conttype = "button" ):
@@ -184,7 +173,16 @@ def handlecontrol( number, value, conttype = "button" ):
 
 
 def senddata( message ):
+    """
+    :param message: the data that gets sent to the arduino, to be handled and control the motors
+    :return: no return
+    """
     sock.sendto( message, ( IP, PORT))
+
+
+
+# This while loop is like the main for client.py. Python files don't necessarily need mains when used like this.
+
 
 while True:  # during control loop
     joystick = pygame.joystick.Joystick(0)
